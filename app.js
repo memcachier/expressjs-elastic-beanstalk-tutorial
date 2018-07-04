8dcfde7eaa0920057469289bb3f31f759e49e235
Lindsey Jacks <linzjax@gmail.com>
2018-07-04 14:31:21 -0400

    Updating and cleaning app.js

--- app.js
+++ app.js
@@ -10,24 +10,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 /* ADD THE APP.JS CODE HERE! */
 
 // Super simple algorithm to find largest prime <= n
@@ -64,5 +46,21 @@ app.get('/', function (req, res) {
 
 
 /* END DIY CODE*/
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
